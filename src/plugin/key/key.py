707f892c2a9c9307181106ec9cf200fca6bc03a6
ntd284 <nguyentuanduong7@example.com>
2024-02-28 17:09:34 +0700

    gitadd

--- src/plugin/key/key.py
+++ src/plugin/key/key.py
@@ -1,6 +1,5 @@
 class token():
     def token_tele():
-        # return "6477758459:AAF2k84aXnwA0YqAjZ74eLRigAlS21a2YF0"
         return "6628261569:AAEfqd4Byy1cTYHIgHcorqQ3PTxtod5bD0Q"
     def deviceID():
         return "3C:06:30:54:C6:C3"
@@ -10,7 +9,5 @@ class token():
         return "http://127.0.0.1:8000"
     def dictionary_file():
         return "/usr/app/src/telestockbot/file"
-        # return "/Users/macos/Documents/Python/NTspace/telestockbot/telestockbot/file"
-        # return "/root/telestockbot/telestockbot/file"
     def chat_admin_id():
         return 5114574365
